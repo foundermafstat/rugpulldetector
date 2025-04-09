d83cf6a9e5d5afbc531bb9913d2c6c1380bc667c
moneymessgame <moneymessgame@gmail.com>
2025-04-09 08:26:53 +0200

    fix problems

--- src/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './error.helpers'
-export * from './log.helpers'
-export * from './validation.helpers'
--- src/modules/app-module/controller/index.ts
+++
@@ -1,4 +0,0 @@
-export * as healthCheckController from './health-check'
-export * as homeController from './home'
-export * as versionController from './version'
-export * as apiDocsController from './api-docs'
